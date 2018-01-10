f6c5f84e04b8393ff2dea3062f9ee8719b7f3054
JosephJvB <jo-van-bo@hotmail.com>
2018-01-10 22:19:54 +1300

    baby steps

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,11 +4,12 @@ import h from 'react-hyperscript'
 import Hypertester from './Hypertester'
 
 const App = () => {
+  const str = 'joe likes to learn and he learns best by doing'
   return (
     h(Fragment, [
       h('h1', {'className': 'title is-1'}, 'react'),
       h('h1', { 'className': 'title is-4' }, 'is cool'),
-      h(Hypertester)
+      h(Hypertester, {'str': str})
     ])
   )
 }
--- client/components/Hypertester.jsx
+++ client/components/Hypertester.jsx
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react' // try Fragment once HS working
 import h from 'react-hyperscript'
 
-const App = () => {
+const App = (props) => {
   return (
     h(Fragment, [
       'is this where the text goes',
       h('p', 'what if I wanted a child element'),
       h('p', 'what if I wanted a second child element'),
-      h('button', {'className': 'button is-large is-success'}, 'hi im a button')
+      h('button',
+        {'className': 'button is-large is-success',
+          'onClick': () => global.console.log('radical')},
+        props.str)
     ])
   )
 }
