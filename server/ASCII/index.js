92a133e01e881b4e0564d5f98fc211ffcdc89df6
JosephJvB <jo-van-bo@hotmail.com>
2018-01-12 11:11:18 +1300

    little tidy more comments

--- server/ASCII/index.js
+++ server/ASCII/index.js
@@ -1,11 +1,12 @@
 const fs = require('mz/fs')
+const { console } = global
 
 fs.readFile(`${__dirname}/joe.txt`, 'ascii')
-  .then(joe => global.console.log(joe))
-  .catch(err => global.console.log(err))
+  .then(joe => console.log(joe))
+  .catch(err => console.log(err))
 
 // http://www.glassgiant.com/ascii/ seems like a decent place for smallish ascii
-// wow what a mess
+// wow what a mess - found this after I'd already figured it out :( https://stackoverflow.com/questions/6456864/why-does-node-js-fs-readfile-return-a-buffer-instead-of-string
 // OK I GOT IT I think it's because I was doing the readFile without any encoding.. adding utf8 meant i didnt need toString
 // Object.keys(fish) = array with ~ 30000 indexes... Why is my text file this weird object??
 // fish instanceof Object = true. that's weird
