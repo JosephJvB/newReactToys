b646de9cc10b9da914f3d15c4f76cca31e6acbdf
JosephJvB <jo-van-bo@hotmail.com>
2018-01-11 16:50:19 +1300

    but how to do conditional HS

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,24 @@
-import React, { Fragment } from 'react'
+import { Fragment } from 'react'
 import h from 'react-hyperscript'
 
 import Hypertester from './Hypertester'
 
 const App = () => {
+  const yaCompLookin = () => h(Fragment, ['hehe this is a component but its really just a callback saved to a variable hehe'])
   const str = 'joe likes to learn and he learns best by doing'
   return (
     h(Fragment, [
       h('h1', {'className': 'title is-1'}, 'react'),
       h('h1', { 'className': 'title is-4' }, 'is cool'),
-      h(Hypertester, {'str': str})
+      h(Hypertester, {'str': str}),
+      h(yaCompLookin),
+      h(Hypertester, {'str': 'Ash catches pokemon and wants to be the best'})
     ])
   )
 }
+
+// element properties are cool
+// components rendering components are cool
+// how to do conditionals in hyperscript?
 
 export default App
--- client/components/Hypertester.jsx
+++ client/components/Hypertester.jsx
@@ -1,20 +1,27 @@
-import React, { Fragment } from 'react' // try Fragment once HS working
+import { Fragment } from 'react' // try Fragment once HS working
 import h from 'react-hyperscript'
+const { console } = global
 
-const App = (props) => {
+const Hypertester = (props) => {
   const label = props.str[0].toUpperCase() + props.str.substring(1, 3)
   const msg = props.str.slice(3)
   return (
     h(Fragment, [
       'is this where the text goes',
-      h('p', 'what if I wanted a child element'),
-      h('p', 'what if I wanted a second child element'),
+      h('p', ['what if I wanted a child element', ' & ', 'what if that child has a brother']),
+      h(Fragment, ['what if I wanted a second child element',
+        h('p', 'what if that child had a child')]),
       h('button',
         {'className': 'button is-large is-success',
-          'onClick': () => global.console.log(`he${msg}`)},
-        label)
+          'onClick': () => console.log(`he${msg}`)},
+        `${label}:`)
     ])
   )
 }
 
-export default App
+// nested fragments are pretty cool
+// onclicks are cool
+// string methods are cool
+// element properties are cool
+
+export default Hypertester
